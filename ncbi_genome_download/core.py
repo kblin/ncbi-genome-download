c79ff8e8ef8c6a5477c6d66024cdc424e3b37d6f
Kai Blin <kblin@biosustain.dtu.dk>
2016-05-05 13:08:48 +0200

    core: Add actual code downloading logic
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- ncbi_genome_download/core.py
+++ ncbi_genome_download/core.py
@@ -1,5 +1,8 @@
 import csv
+import errno
+import hashlib
 import logging
+import os
 import requests
 from StringIO import StringIO
 
@@ -22,6 +25,8 @@ def _download(section, domain, uri, output):
     '''Download a specified domain form a section'''
     summary = get_summary(section, domain, uri)
     entries = parse_summary(summary)
+    for entry in entries:
+        download_entry(entry, section, domain, uri, output)
 
 
 def get_summary(section, domain, uri):
@@ -44,3 +49,116 @@ def parse_summary(summary):
 
     reader = csv.DictReader(summary, dialect='excel-tab')
     return reader
+
+
+def download_entry(entry, section, domain, uri, output):
+    '''Download an entry from the summary file'''
+    logging.info('Downloading record %r', entry['assembly_accession'])
+    full_output_dir = create_dir(entry, section, domain, output)
+    checksums = grab_checksums_file(entry)
+
+    # TODO: Only write this when the checksums file changed
+    with open(os.path.join(full_output_dir, 'MD5SUMS'), 'w') as fh:
+        fh.write(checksums)
+
+    parsed_checksums = parse_checksums(checksums)
+    if has_gbk_file_changed(full_output_dir, parsed_checksums):
+        download_gbk_file(entry, full_output_dir, parsed_checksums)
+
+
+def create_dir(entry, section, domain, output):
+    '''Create the output directory for the entry if needed'''
+    full_output_dir = os.path.join(output, section, domain, entry['assembly_accession'])
+    try:
+        os.makedirs(full_output_dir)
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(full_output_dir):
+            pass
+        else:
+            raise
+
+    return full_output_dir
+
+
+def grab_checksums_file(entry):
+    '''Grab the checksum file for a given entry'''
+    http_url = convert_ftp_url(entry['ftp_url'])
+    full_url = '{}/md5checksums.txt'.format(http_url)
+    r = requests.get(full_url)
+    return r.text
+
+
+def convert_ftp_url(url):
+    '''Convert FTP to HTTP URLs'''
+    return url.replace('ftp://', 'http://', 1)
+
+
+def parse_checksums(checksums_string):
+    '''Parse a file containing checksums and filenames'''
+    checksums_list = []
+    for line in checksums_string.split('\n'):
+        try:
+            checksum, filename = line.split()
+            # strip leading ./
+            if filename.startswith('./'):
+                filename = filename[2:]
+            checksums_list.append({'checksum': checksum, 'file': filename})
+        except ValueError:
+            logging.debug('Skipping over unexpected checksum line %r', line)
+            continue
+
+    return checksums_list
+
+
+def has_gbk_file_changed(directory, checksums):
+    '''Check if the checksum of a given gbk file has changed'''
+    filename, expected_checksum = get_name_and_checksum(checksums, '_genomic.gbff.gz')
+    full_filename = os.path.join(directory, filename)
+    # if file doesn't exist, it has changed
+    if not os.path.isfile(full_filename):
+        return True
+
+    actual_checksum = md5sum(full_filename)
+    return expected_checksum != actual_checksum
+
+
+def get_name_and_checksum(checksums, end):
+    '''Extract a full filename and checksum from the checksums list for a file ending in given end'''
+    for entry in checksums:
+        if not entry['file'].endswith(end):
+            # wrong file
+            continue
+        filename = entry['file']
+        expected_checksum = entry['checksum']
+        return filename, expected_checksum
+
+
+def md5sum(filename):
+    '''Calculate the md5sum of a file and return the hexdigest'''
+    hash_md5 = hashlib.md5()
+    with open(filename, 'rb') as fh:
+        for chunk in iter(lambda: fh.read(4096), b''):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def download_gbk_file(entry, directory, checksums):
+    '''Download and verirfy a given gbk file'''
+    filename, expected_checksum = get_name_and_checksum(checksums, '_genomic.gbff.gz')
+    base_url = convert_ftp_url(entry['ftp_url'])
+    full_url = '{}/{}'.format(base_url, filename)
+    local_file = os.path.join(directory, filename)
+
+    r = requests.get(full_url, stream=True)
+
+    with open(local_file, 'wb') as fh:
+        for chunk in r.iter_content(4096):
+            fh.write(chunk)
+
+    actual_checksum = md5sum(local_file)
+    if actual_checksum != expected_checksum:
+        logging.error('Checksum mismatch for %r. Expected %r, got %r',
+                local_file, expected_checksum, actual_checksum)
+        return False
+
+    return True
--- tests/test_core.py
+++ tests/test_core.py
@@ -64,3 +64,128 @@ def test_parse_summary():
         reader = core.parse_summary(fh)
         first = reader.next()
         assert first.has_key('assembly_accession')
+
+
+def test_download_entry():
+    pass
+
+
+def test_create_dir(tmpdir):
+    entry = {'assembly_accession': 'FAKE0.1' }
+    output = tmpdir.mkdir('output')
+    ret = core.create_dir(entry, 'refseq', 'bacteria', str(output))
+
+    expected = output.join('refseq', 'bacteria', 'FAKE0.1')
+    assert expected.check()
+    assert ret == str(expected)
+
+
+def test_create_dir_exists(tmpdir):
+    entry = {'assembly_accession': 'FAKE0.1' }
+    output = tmpdir.mkdir('output')
+    output.mkdir('refseq').mkdir('bacteria').mkdir('FAKE0.1')
+    core.create_dir(entry, 'refseq', 'bacteria', str(output))
+
+
+def test_create_dir_isfile(tmpdir):
+    entry = {'assembly_accession': 'FAKE0.1' }
+    output = tmpdir.mkdir('output')
+    output.join('refseq', 'bacteria', 'FAKE0.1').write('foo', ensure=True)
+    with pytest.raises(OSError):
+        core.create_dir(entry, 'refseq', 'bacteria', str(output))
+
+
+def test_grab_checksums_file(req):
+    req.get('http://ftp.ncbi.nih.gov/genomes/all/FAKE0.1/md5checksums.txt', text='test')
+    entry = {'ftp_url': 'ftp://ftp.ncbi.nih.gov/genomes/all/FAKE0.1' }
+    ret = core.grab_checksums_file(entry)
+    assert ret == 'test'
+
+
+def test_parse_checksums():
+    checksums_string = '''\
+d3c2634cedd0efe05cbf8a5f5384d921  ./GCF_000009605.1_ASM960v1_feature_table.txt.gz
+42c1bb1447aea2512a17aeb3645b55e9  ./GCF_000009605.1_ASM960v1_genomic.fna.gz
+8a685d49d826c4f0ad05152e906f3250  ./GCF_000009605.1_ASM960v1_genomic.gbff.gz
+e2d9e1cfa085cb462a73d3d2d2c22be5  ./GCF_000009605.1_ASM960v1_genomic.gff.gz
+d8ce7c80d457e012f9d368a4673dea65  ./GCF_000009605.1_ASM960v1_protein.faa.gz
+620a09de4286f66113317456c0dc8f66  ./GCF_000009605.1_ASM960v1_protein.gpff.gz
+'''
+    expected = [
+        {'checksum': 'd3c2634cedd0efe05cbf8a5f5384d921', 'file': 'GCF_000009605.1_ASM960v1_feature_table.txt.gz'},
+        {'checksum': '42c1bb1447aea2512a17aeb3645b55e9', 'file': 'GCF_000009605.1_ASM960v1_genomic.fna.gz'},
+        {'checksum': '8a685d49d826c4f0ad05152e906f3250', 'file': 'GCF_000009605.1_ASM960v1_genomic.gbff.gz'},
+        {'checksum': 'e2d9e1cfa085cb462a73d3d2d2c22be5', 'file': 'GCF_000009605.1_ASM960v1_genomic.gff.gz'},
+        {'checksum': 'd8ce7c80d457e012f9d368a4673dea65', 'file': 'GCF_000009605.1_ASM960v1_protein.faa.gz'},
+        {'checksum': '620a09de4286f66113317456c0dc8f66', 'file': 'GCF_000009605.1_ASM960v1_protein.gpff.gz'},
+    ]
+
+    ret = core.parse_checksums(checksums_string)
+    assert ret == expected
+
+
+def test_has_gbk_file_changed_no_file(tmpdir):
+    checksums = [
+        {'checksum': 'fake', 'file': 'skipped'},
+        {'checksum': 'fake', 'file': 'fake_genomic.gbff.gz'},
+    ]
+    assert core.has_gbk_file_changed(str(tmpdir), checksums)
+
+
+def test_has_gbk_file_changed(tmpdir):
+    checksums = [
+        {'checksum': 'fake', 'file': 'skipped'},
+        {'checksum': 'fake', 'file': 'fake_genomic.gbff.gz'},
+    ]
+    fake_file = tmpdir.join(checksums[-1]['file'])
+    fake_file.write('foo')
+    assert fake_file.check()
+    assert core.has_gbk_file_changed(str(tmpdir), checksums)
+
+
+def test_has_gbk_file_changed_unchanged(tmpdir):
+    fake_file = tmpdir.join('fake_genomic.gbff.gz')
+    fake_file.write('foo')
+    assert fake_file.check()
+    checksum = core.md5sum(str(fake_file))
+
+    checksums = [
+        {'checksum': 'fake', 'file': 'skipped'},
+        {'checksum': checksum, 'file': fake_file.basename},
+    ]
+
+    assert core.has_gbk_file_changed(str(tmpdir), checksums) == False
+
+
+def test_md5sum():
+    expected = '74d72df33d621f5eb6300dc9a2e06573'
+    filename = _get_file('partial_summary.txt')
+    ret = core.md5sum(filename)
+    assert ret == expected
+
+
+def test_download_gbk_file(req, tmpdir):
+    entry = {'ftp_url': 'ftp://fake/path'}
+    fake_file = tmpdir.join('fake_genomic.gbff.gz')
+    fake_file.write('foo')
+    assert fake_file.check()
+    checksum = core.md5sum(str(fake_file))
+    checksums = [{'checksum': checksum, 'file': fake_file.basename}]
+    dl_dir = tmpdir.mkdir('download')
+    req.get('http://fake/path/fake_genomic.gbff.gz', text=fake_file.read())
+
+
+    assert core.download_gbk_file(entry, str(dl_dir), checksums)
+
+
+def test_download_gbk_file_mismatch(req, tmpdir):
+    entry = {'ftp_url': 'ftp://fake/path'}
+    fake_file = tmpdir.join('fake_genomic.gbff.gz')
+    fake_file.write('foo')
+    assert fake_file.check()
+    checksums = [{'checksum': 'fake', 'file': fake_file.basename}]
+    dl_dir = tmpdir.mkdir('download')
+    req.get('http://fake/path/fake_genomic.gbff.gz', text=fake_file.read())
+
+
+    assert core.download_gbk_file(entry, str(dl_dir), checksums) == False
