b4ca5cbfeeb2bc4b2cde8ef5b669817903d0b356
Kai Blin <kblin@biosustain.dtu.dk>
2016-06-01 11:14:12 +0200

    summary: Fix pylint warnings
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- ncbi_genome_download/summary.py
+++ ncbi_genome_download/summary.py
@@ -1,7 +1,9 @@
 '''Parse NCBI RefSeq/GenBank summary files'''
 
 
+# pylint: disable=too-few-public-methods
 class SummaryReader(object):
+    '''An Iterator-like class for assembly summary files'''
     def __init__(self, infile):
         self._file = infile
         line = ''
@@ -17,6 +19,7 @@ class SummaryReader(object):
         return self
 
     def next(self):
+        '''Return the next entry'''
         return self.__next__()
 
     def __next__(self):
@@ -28,3 +31,4 @@ class SummaryReader(object):
         for i, val in enumerate(parts):
             entry[self._fields[i]] = val
         return entry
+# pylint: enable=too-few-public-methods
