ca682b8647a29959d0be715228dd8511b29f25d1
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-02-10 14:35:17 +0100

    Bug fixes in Enums, some renamings, more doc

--- ncbi_genome_download/__init__.py
+++ ncbi_genome_download/__init__.py
@@ -1,18 +1,17 @@
 """Download genome files from the NCBI"""
 from ncbi_genome_download.core import (
     download,
-    NCBI_URI,
-    SUPPORTED_DOMAINS,
-    FORMAT_NAME_MAP,
-    ASSEMBLY_LEVEL_MAP,
+    SUPPORTED_TAXONOMIC_GROUPS,
+    EFormats,
+    EAssemblyLevels,
     EDefaults
 )
+
 __version__ = '0.2.3'
 __all__ = [
     'download',
-    'NCBI_URI',
-    'SUPPORTED_DOMAINS',
-    'FORMAT_NAME_MAP',
-    'ASSEMBLY_LEVEL_MAP',
+    'SUPPORTED_TAXONOMIC_GROUPS',
+    'EFormats',
+    'EAssemblyLevels',
     'EDefaults'
 ]
