8f1c34fcb85674e7bbb608d453a9beaa51d75c27
Ryan Wick <rrwick@gmail.com>
2017-10-24 13:32:53 +1100

    Runner script for convenience

---
+++ ncbi-genome-download-runner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Convenience wrapper for running ncbi-genome-download directly from source tree.
+"""
+
+from ncbi_genome_download.__main__ import main
+
+if __name__ == '__main__':
+    main()
+
