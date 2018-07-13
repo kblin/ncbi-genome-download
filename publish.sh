5ff1e3ed23a6bc325d173fc5548bbcb1b8c5851f
Kai Blin <kblin@biosustain.dtu.dk>
2018-07-13 11:38:00 +0200

    chore: setup.py finally understands markdown natively
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- publish.sh
+++ publish.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-PANDOC=pandoc
-
-${PANDOC} -f markdown_github -t rst -o README.rst README.md
 rm -rf dist
 python setup.py sdist bdist_wheel
 twine upload dist/*
-rm README.rst
